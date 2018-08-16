2313c1f0e94221da98783a38aebfa4d56839ba43
Ubuntu <ubuntu@ip-172-31-39-100.us-east-2.compute.internal>
2018-08-16 14:16:13 +0000

    Initial

---
+++ installGraphite.sh
@@ -0,0 +1,69 @@
+export login="graphite"
+export password="graphite"
+export email="lrochette@electric-cloud.com"
+export VERSION=0.9.10
+
+set -x
+echo I am provisioning...
+#date > /etc/vagrant_provisioned_at
+apt-get update
+echo "America/Los_Angeles" | sudo tee /etc/timezone
+ntpdate -u http://pool.ntp.org
+dpkg-reconfigure --frontend noninteractive tzdata
+
+# Remove apparmor
+/etc/init.d/apparmor stop
+apt-get --purge remove -y apparmor apparmor-utils libapparmor-perl libapparmor1
+
+# install the needed package
+echo "Install Required packages"
+apt-get install -y graphite-web graphite-carbon
+
+# install and configure DB
+apt-get install -y postgresql libpq-dev python-psycopg2
+apt-get install -y expect
+sudo -u postgres psql << DB
+CREATE USER graphite WITH PASSWORD 'graphite';
+CREATE DATABASE graphite WITH OWNER graphite;
+DB
+
+cp files/local_settings.py /etc/graphite/local_settings.py
+graphite-manage syncdb --noinput
+graphite-manage createsuperuser --username="${login}" --email="${email}" --noinput
+expect << DONE
+spawn graphite-manage changepassword "${login}"
+expect "Password: "
+send -- "${password}\r"
+expect "Password (again): "
+send -- "${password}\r"
+expect eof
+DONE
+
+cp files/graphite-carbon /etc/default/graphite-carbon
+cp files/carbon.conf /etc/carbon/carbon.conf
+cp files/storage-schemas.conf /etc/carbon/storage-schemas.conf
+cp files/storage-aggregation.conf /etc/carbon/storage-aggregation.conf
+service carbon-cache start
+
+# install and configure apache
+apt-get install -y apache2 libapache2-mod-wsgi
+a2dissite 000-default
+cp files/apache2-graphite.conf /etc/apache2/sites-available
+a2ensite apache2-graphite
+service apache2 reload
+
+#install and configure statsd
+apt-get install -y git nodejs devscripts debhelper
+cd /opt
+git clone https://github.com/etsy/statsd.git
+cd statsd
+dpkg-buildpackage
+cd ..
+service carbon-cache stop
+dpkg -i statsd*.deb
+service statsd stop
+
+cp files/localConfig.js /etc/statsd/localConfig.js
+cp files/storage-aggregation.conf /etc/carbon/storage-aggregation.conf
+service carbon-cache start
+service statsd       start    
